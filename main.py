9fbe4fe20aa9e23be4dabba0fb4c5063fdb3e518
lenier.villalba <lenier.villalba@gmail.com>
2025-06-25 01:49:19 +0000

    forms update

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return send_file('src/index.html')
+    return render_template('pages/auth/auth-login.html', title="Inicio de Sesión")
+
+@app.route('/')
+def login():
+    return render_template('pages/auth/auth-login.html', title="Inicio de Sesión")
+
+@app.route('/register')
+def register():
+    return render_template('pages/auth/auth-register.html', title="Regístrate")
+
+@app.route('/recoverpw')
+def recoverpw():
+    return render_template('pages/auth/auth-recoverpw.html', title="Recupera tu contraseña")    
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
