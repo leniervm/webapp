bdd411d4d31f6f6645aea7eb4dfc135ba31b1525
lenier.villalba <lenier.villalba@gmail.com>
2025-06-25 02:09:46 +0000

    auth-pages update

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('pages/auth/auth-login.html', title="Inicio de Sesión")
+    return render_template('pages/index.html', title="Dashboard")
 
-@app.route('/')
+@app.route('/login')
 def login():
     return render_template('pages/auth/auth-login.html', title="Inicio de Sesión")
 
@@ -18,7 +18,19 @@ def register():
 
 @app.route('/recoverpw')
 def recoverpw():
-    return render_template('pages/auth/auth-recoverpw.html', title="Recupera tu contraseña")    
+    return render_template('pages/auth/auth-recoverpw.html', title="Recupera tu contraseña")
+
+@app.route('/confirm_mail')
+def confirm_mail():
+    return render_template('pages/auth/auth-confirm_mail.html', title="Confirma tu email")    
+
+@app.route('/lock_screen')
+def lock_screen():
+    return render_template('pages/auth/auth-lock_screen.html', title="Lock screen")
+
+@app.route('/logout')
+def logout():
+    return render_template('pages/auth/auth-logout.html', title="Sesión cerrada")
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
